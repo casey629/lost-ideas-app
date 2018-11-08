a18ecaefd27cfb3f96693b3545b430a820dbdc6b
Ergin <caseyjane629@gmail.com>
2018-11-08 16:54:56 +0300

    Idea class ve constructor oluşturuldu.

--- app/src/main/java/com/app/ergin/lostideasapp/Idea.kt
+++ app/src/main/java/com/app/ergin/lostideasapp/Idea.kt
@@ -1,2 +1,15 @@
 package com.app.ergin.lostideasapp
 
+class Idea {
+    var id : Int = 0
+    var name: String = ""
+    var type: String = ""
+
+    constructor(name: String,type: String){
+        this.name = name
+        this.type = type
+    }
+    constructor(){
+
+    }
+}
